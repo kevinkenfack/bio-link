9a3c62b9797a1c77480b1e879288fe31e295e2db
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-11-29 17:13:09 +0100

    increase desing ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import NeoBiolink from './components/NeoBiolink'
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <NeoBiolink />
