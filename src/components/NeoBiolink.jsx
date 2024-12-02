73844aae0a52f9b90110b6d8ba01b749be4a2b95
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-12-02 07:04:16 +0100

    update prject and socials link

--- src/components/NeoBiolink.jsx
+++ src/components/NeoBiolink.jsx
@@ -35,7 +35,7 @@ const NeoBiolink = () => {
     },
     {
       title: 'Analytics Rank',
-      description: 'A simplified, intuitive analytics SaaS for tracking website traffic and user insights—no complex setup required.',
+      description: 'A simplified, intuitive analytics SaaS for tracking website traffic and user insights no complex setup required.',
       url: 'https://kevin.tagueacademy.com/',
       icon: BriefcaseIcon
     },
@@ -64,6 +64,7 @@ const NeoBiolink = () => {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: index * 0.1, duration: 0.5 }}
+        target="_blank"
         className={`
           block w-full p-3
           rounded-xl 
